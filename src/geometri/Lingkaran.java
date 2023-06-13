0ac87049d418550e9f61ce2cc8bd6863ddfb049a
ninaproject <ninabrays12@gmail.com>
2023-06-12 15:56:41 -0900

    Membuat class Lingkaran dan PersegiPanjang

---
+++ src/geometri/Lingkaran.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometri;
+
+public class Lingkaran {
+    
+    //membuat variabel
+    private double luas, keliling;
+    
+    //constructor
+    public Lingkaran(double luas, double keliling) {
+        this.luas = luas;
+        this.keliling = keliling;
+    }
+    
+    double  luas(){
+        return 
+    }
+}
---
+++ src/geometri/PersegiPanjang.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometri;
+
+public class PersegiPanjang {
+    //membuat variabel
+    private double bil1, bil2;
+    
+    //constructor
+    public Lingkaran(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    double  luas(){
+        return 
+    }
+
+}
